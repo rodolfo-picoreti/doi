5c953b7258b1572f570608a142cc114a805ead2a
picoreti <rodolfo.picoreti@gmail.com>
2017-07-17 18:26:29 -0300

    fixed parser

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const wuzzy = require('wuzzy');
 
 const limiter = new Bottleneck(5, 2000);
 
-let {data} = parseFiles(__dirname+'/pubs.csv', { delimiter: ";", newline: "\r\n" });
+let {data} = parseFiles(__dirname+'/pubs.csv', { delimiter: ";" });
+data = _.filter(data, (record) => record.length > 1);
 
 let fields = data[0];
 fields.push('DOI');
@@ -15,15 +16,15 @@ fields.push('DOI');
 let indexed = _.map(data.slice(1), (array, index) => _.concat(index, array));
 let unique = _.uniqBy(indexed, fields.indexOf('Titulo') + 1);
 
+console.log(`Found ${unique.length} unique titles`);
+
 _.map(unique, (record) => {
   let index = record[0];
   let title = record[fields.indexOf('Titulo') + 1];
   let author = record[fields.indexOf('Nome Autor') + 1];
   let year = record[fields.indexOf('Ano') + 1];
   let code = record[fields.indexOf('Codigo Trabalho') + 1];
-  
-  console.log(`[${code}] ${title.slice(0,49)}...`);
-  
+   
   limiter.schedule(rp, {
     uri: 'https://api.crossref.org/works',
     qs: {
