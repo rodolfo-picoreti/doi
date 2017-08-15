bc7edee4aacf1b5c9bb8db7accbe3d5017f27669
picoreti <rodolfo.picoreti@gmail.com>
2017-08-15 17:05:22 -0300

    improved score

--- app.js
+++ app.js
@@ -5,44 +5,105 @@ const rp = require('request-promise');
 const Bottleneck = require("bottleneck"); 
 const wuzzy = require('wuzzy');
 
-const limiter = new Bottleneck(5, 2000);
+const REQUEST_MAX_PARALLELLISM = process.env.REQUEST_MAX_PARALLELLISM || 5;
+const DELAY_BETWEEN_REQUESTS = process.env.DELAY_BETWEEN_REQUESTS || 500;
+const AUTHOR_FIELD = process.env.AUTHOR_FIELD || "NomeAutor";
+const CONTAINER_FIELD = process.env.CONTAINER_FIELD || "NomePeriodico";
+const TITLE_FIELD = process.env.TITLE_FIELD || "Titulo";
+const YEAR_FIELD = process.env.YEAR_FIELD || "Ano";
+const CSV_DELIMITER = process.env.CSV_DELIMITER || ";";
+const CSV_IN_PATH = process.argv[2] || "publications.csv";
+const CSV_OUT_PATH = process.argv[3] || "publications_xref.csv";
 
-let {data} = parseFiles(__dirname+'/pubs.csv', { delimiter: ";" });
-data = _.filter(data, (record) => record.length > 1);
+const limiter = new Bottleneck(REQUEST_MAX_PARALLELLISM, DELAY_BETWEEN_REQUESTS);
 
-let fields = data[0];
-fields.push('DOI');
+let {data} = parseFiles(CSV_IN_PATH, { delimiter: CSV_DELIMITER });
+let nonEmpty = _.filter(data, (record) => record.length > 1);
+let fields = _.concat('Index', nonEmpty[0], 'DOI', 'Abstract');
+let indexed = _.map(nonEmpty.slice(1), (array, index) => _.concat(index, array));
+let groupped = _.groupBy(indexed, fields.indexOf(TITLE_FIELD));
 
-let indexed = _.map(data.slice(1), (array, index) => _.concat(index, array));
-let unique = _.uniqBy(indexed, fields.indexOf('Titulo') + 1);
+function appendToFile(array) {
+  fs.appendFileSync(CSV_OUT_PATH, unparse(array, {delimiter: ';'}) + ';\r\n'); 
+};
 
-console.log(`Found ${unique.length} unique titles`);
+function computeScore(our, rcv) {
+  const scoreFunction = {
+    title: (our, rcv) => wuzzy.levenshtein(our.toLowerCase(), rcv.toLowerCase()),
+    container(our, rcv) {
+      return wuzzy.levenshtein(our.toLowerCase(), rcv.toLowerCase())
+    },
+    year(our, rcv) {
+      return our == rcv ? 1.5 : 0.5;
+    },
+    author(our, rcv) {
+      return our.toLowerCase().indexOf(rcv.toLowerCase()) != -1 ? 1.5 : 0.5;
+    }
+  };
 
-_.map(unique, (record) => {
-  let index = record[0];
-  let title = record[fields.indexOf('Titulo') + 1];
-  let author = record[fields.indexOf('Nome Autor') + 1];
-  let year = record[fields.indexOf('Ano') + 1];
-  let code = record[fields.indexOf('Codigo Trabalho') + 1];
-   
-  limiter.schedule(rp, {
+  let score = _.reduce(rcv, (total, value, key) => {
+    if (_.isNull(value)) {
+      return { score: total.score, nulls: _.concat(total.nulls, key) };
+    }
+    return { score: scoreFunction[key](our[key], value)*total.score, nulls: total.nulls };
+  }, { score: 1.0, nulls: [] });
+  
+  if (!_.isEmpty(score.nulls)) {
+    console.warn(`[!] "${our.title.slice(0,49)} [...]" fields ${_.join(score.nulls, ', ')} are undefined`);
+  }
+  
+  return score.score * 100.0;
+} 
+
+appendToFile([fields]);
+
+let found = 0;
+let total = Object.keys(groupped).length;
+console.log(`[x] Processing ${total} papers... This will take approx. ${total*DELAY_BETWEEN_REQUESTS/60000} m`);
+
+Promise.all(_.map(groupped, (group) => {
+  let our = {
+    title: group[0][fields.indexOf(TITLE_FIELD)],
+    container: group[0][fields.indexOf(CONTAINER_FIELD)],
+    author: group[0][fields.indexOf(AUTHOR_FIELD)],
+    year: group[0][fields.indexOf(YEAR_FIELD)]  
+  };
+
+  return limiter.schedule(rp, {
     uri: 'https://api.crossref.org/works',
     qs: {
-      'query.title': title,
-      'query.author': author,
+      'query.title': our.title,
+      'query.author': our.author,
       'rows': 1
     },
     json: true
   }).then((result) => {
     let {status, message:{items}} = result;
-    let similarity = wuzzy.levenshtein(title, items[0].title[0]);
-    data[index+1].push(items[0].DOI);
-    data[index+1].push(similarity);
-    console.log(`[${index}][${code}] ${title.slice(0,49)}... => (${status}, ${similarity})`);
-  });
-});
+    let rcv = {
+      title: _.get(items[0], 'title[0]', null),
+      container: _.get(items[0], 'container-title[0]', null),
+      year: _.get(items[0], 'issued.date-parts[0][0]', null),
+      author: _.get(items[0], 'author[0].family', null)
+    };
 
-limiter.on('idle', () => {
-  console.log(`All done... Saving`);
-  fs.writeFileSync('pubs2.csv', unparse(data, {delimiter: ';'}));
-});
+    let score = computeScore(our, rcv);
+    console.log(`[${status}] "${our.title.slice(0,49)} [...]" ${score.toFixed(2)}`);
+    
+    if (score > 40.0) {
+      ++found;
+      group = _.map(group, (record) => {
+        return _.concat(record, _.get(items[0], 'DOI', 'Not found'), _.get(items[0], 'abstract', 'Not found'));
+      });
+    } else {
+      group = _.map(group, (record) => {
+        return _.concat(record, 'Not found', 'Not found');
+      });
+    }
+
+    appendToFile(group);
+  });
+})).catch((e) => {
+  console.log(`[!] Exception: ${e}`);
+}).then(() => {
+  console.log(`[x] All done ... found ${found}/${total}`);
+}); 
